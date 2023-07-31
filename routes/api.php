84ec40770a45250174972b0cf11f87b1a9264e81
tzptzptzptzptzp <tzp2539@gmail.com>
2023-07-31 23:05:21 +0900

    ✨ APIのルーティングを設定

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Post Routing
+ */
+
+Route::get('/posts', 'App\Http\Controllers\PostController@index');
+Route::post('/posts', 'App\Http\Controllers\PostController@store');
+Route::get('/posts/{id}', 'App\Http\Controllers\PostController@show');
+Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update');
+Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@destroy');
