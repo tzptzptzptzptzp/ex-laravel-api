94ae32f42233e6d99bfc099becc5b0e7bb45d7ba
tzptzptzptzptzp <tzp2539@gmail.com>
2023-07-31 23:05:39 +0900

    ✨ APIの処理を実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Post::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Post::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+        return $post;
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return response()->json(null, 204);
+    }
 }
