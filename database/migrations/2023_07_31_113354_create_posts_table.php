8b4110cd82139f194df58a8d114de1ff5193c756
tzptzptzptzptzp <tzp2539@gmail.com>
2023-07-31 21:16:37 +0900

    ✨ Postにタイトル、本文、公開状態を追加

--- database/migrations/2023_07_31_113354_create_posts_table.php
+++ database/migrations/2023_07_31_113354_create_posts_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('title');
+            $table->text('content');
+            $table->boolean('published')->default(false);
         });
     }
 
